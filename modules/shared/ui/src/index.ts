b8b20e193342d53525c4db765f4425b460a7afd5
DocCobra <daniele@torrini.org>
2024-08-12 01:52:20 +0200

    Button with action :active

--- modules/shared/ui/src/index.ts
+++ modules/shared/ui/src/index.ts
@@ -1,4 +1,5 @@
 export * from './lib/ui/ui.component';
 export * from './mls-button/mls-button.component'; 
+export * from './mls-card/mls-card.component'; 
 export * from './mls-char-list/mls-char-list.component'; 
 export * from './mls-legion-grid/mls-legion-grid.component'; 
---
+++ modules/shared/ui/src/mls-card/mls-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'mls-card',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './mls-card.component.html',
+  styleUrl: './mls-card.component.scss',
+})
+export class MlsCardComponent {}
